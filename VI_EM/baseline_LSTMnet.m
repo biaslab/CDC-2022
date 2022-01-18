7dfc56d94fab3e8f9a22d7eaf26904cb6305fcc4
wmkouw <wmkouw@gmail.com>
2022-01-18 21:14:21 +0100

    Adaptation after notebook bugfixes on my end

--- VI_EM/baseline_LSTMnet.m
+++ VI_EM/baseline_LSTMnet.m
@@ -11,13 +11,13 @@ clear all;
 %% Import data from julia notebook
 
 load('train_data.mat')
-load('test_data.mat')
+load('validation_data.mat')
 
 %% Baseline LSTM net
 
 % Define network architecture
 numResponses = 1;
-featureDimension = 1;
+featureDimension = 2;
 numHiddenUnits = 100;
 maxEpochs = 500;
 miniBatchSize = 10;
@@ -36,24 +36,27 @@ options = trainingOptions('adam', ...
     'LearnRateSchedule','piecewise',...
     'LearnRateDropPeriod',100,...
     'Verbose',0,...
-    'ValidationData',[{input_test'} {output_test'}]);
+    'ValidationData',[{u_val} {y_val}]);
 
 % Train using parallel threads
 poolobj = parpool;
-fullNet = trainNetwork(input_train', output_train', Networklayers, options);
+fullNet = trainNetwork(u_trn, y_trn, Networklayers, options);
 delete(poolobj)
 
 % Validate performance
-predictions = predict(fullNet, input_test');
-MSE = mean((output_test' - predictions).^2)
+predictions = predict(fullNet, u_val);
+MSE = mean((y_val - predictions).^2)
 save('LSTMnet.mat', 'fullNet', 'predictions', 'MSE');
 
 % Plot results
 figure; hold on
-N_val = length(output_test);
-plot(1:N_val, output_test', 'LineWidth', 1, 'Color', 'black')
+N_val = length(y_val);
+plot(1:N_val, y_val, 'LineWidth', 1, 'Color', 'black')
 plot(1:N_val, predictions', 'LineWidth', 3, 'Color', 'blue')
-legend({'test output', '1-step preds'})
+legend({'validation output', '1-step preds'})
 xlim([0,N_val])
+title(['LSTM-net: [u_t u_{t-1}] -> y_t' newline 'MSE = ' num2str(MSE)])
+set(gcf, 'Color', 'w')
+exportgraphics(gcf,'LSTMnet.png','Resolution',300)
 
 
