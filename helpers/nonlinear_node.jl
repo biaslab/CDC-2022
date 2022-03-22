80f518f12415dd263cf29eacdf0a696677457e18
Albert Podusenko <albert.podusenko@gmail.com>
2022-03-22 21:22:35 +0100

    Update

--- helpers/nonlinear_node.jl
+++ helpers/nonlinear_node.jl
@@ -110,18 +110,4 @@ end
     return MvNormalWeightedMeanPrecision(prec_mu, prec)
 end
 
-@marginalrule NonlinearNode(:in) (m_out::MvNormalWeightedMeanPrecision, m_in::MvNormalWeightedMeanPrecision, meta::NonlinearMeta) = begin
-    m_in_ = @call_rule NonlinearNode(:in, Marginalisation) (m_out=m_out, m_in=m_in, meta=meta)
-    return prod(ProdAnalytical(), m_in, m_in_)
-end
 
-
-# HELPER
-struct DummyDistribution
-end
-
-Distributions.entropy(dist::DummyDistribution) = ReactiveMP.InfCountingReal(0.0, -1)
-
-@marginalrule typeof(+)(:in1_in2) (m_out::PointMass, m_in1::NormalDistributionsFamily, m_in2::NormalDistributionsFamily, ) = begin
-    return DummyDistribution()
-end
