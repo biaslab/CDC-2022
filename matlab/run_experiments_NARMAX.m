1a5f9fd72026cb8ce050c4a347c8dc53f06b902f
Albert Podusenko <albert.podusenko@gmail.com>
2022-08-24 14:22:40 +0200

    Update project

--- matlab/run_experiments_NARMAX.m
+++ matlab/run_experiments_NARMAX.m
@@ -1,14 +1,14 @@
 close all;
 clear all;
 
-addpath(genpath("../algorithms/ILS-estimator-NARMAX"));
-addpath(genpath("../datasets"));
+addpath(genpath("ILS-estimator-NARMAX"));
+addpath(genpath("realizations"));
 
 %%
 
 % Series of train sizes
 % trn_sizes = 2.^[6:10];
-trn_sizes = [100,200,500,1000];
+trn_sizes = [100,200,400,800];
 num_trnsizes = length(trn_sizes);
 
 % Define transient and test indices
@@ -36,7 +36,7 @@ options.stde = .05;
 
 options.dc = false;
 options.crossTerms = true;
-options.noiseCrossTerms = false;
+options.noiseCrossTerms = true;
 options.normalize = false;
 
 % Number of repetitions
@@ -51,15 +51,15 @@ while r <= num_repeats
     dbox = waitbar(r/num_repeats);
     
     % Generate signal
-%     [yTrain, yTest, uTrain, uTest, eTrain, eTest, system] = gen_signal(options);
+     [yTrain, yTest, uTrain, uTest, eTrain, eTest, system] = gen_signal(options);
     
-%     N_m = size(system.comb,2);
+     N_m = size(system.comb,2);
     
-%     if (max(abs(yTrain)) < 100) && (sum(isnan(yTrain))==0)
+     if (max(abs(yTrain)) < 100) && (sum(isnan(yTrain))==0)
     
         % Write signal to file
-%         save("../datasets/verification/realizations/NARMAXsignal_stde"+num2str(options.stde)+"_degree"+num2str(options.nd)+"_delays"+num2str(M_m)+"_D"+num2str(N_m)+"_r" + string(r) + ".mat", "yTrain", "yTest", "uTrain", "uTest", "eTrain", "eTest", "system", "options")
-        load("../datasets/verification/realizations/NARMAXsignal_stde"+num2str(options.stde)+"_degree"+num2str(options.nd)+"_delays"+num2str(M_m)+"_D"+num2str(22)+"_r" + string(r) + ".mat", "yTrain", "yTest", "uTrain", "uTest", "eTrain", "eTest", "system", "options")
+        save("realizations/NARMAXsignal_stde"+num2str(options.stde)+"_degree"+num2str(options.nd)+"_delays"+num2str(M_m)+"_D"+num2str(N_m)+"_r" + string(r) + ".mat", "yTrain", "yTest", "uTrain", "uTest", "eTrain", "eTest", "system", "options")
+%         load("generated/NARMAXsignal_stde"+num2str(options.stde)+"_degree"+num2str(options.nd)+"_delays"+num2str(M_m)+"_D"+num2str(22)+"_r" + string(r) + ".mat", "yTrain", "yTest", "uTrain", "uTest", "eTrain", "eTest", "system", "options")
         % Preallocate result arrays
         RMS_prd = zeros(1,num_trnsizes);
         RMS_sim = zeros(1,num_trnsizes);
@@ -94,14 +94,14 @@ while r <= num_repeats
         end
         
         % Write results to file
-        save("results/results-NARMAX_ILS_stde"+num2str(options.stde)+"_pol"+num2str(options.nd)+"_delays"+num2str(M_m)+"_D"+num2str(22)+"_degree3_r"+num2str(r)+".mat", "RMS_prd", "RMS_sim")
+        save("ils_results/results-NARMAX_ILS_stde"+num2str(options.stde)+"_pol"+num2str(options.nd)+"_delays"+num2str(M_m)+"_D"+num2str(22)+"_degree3_r"+num2str(r)+".mat", "RMS_prd", "RMS_sim")
         
         results_prd(r,:) = RMS_prd;
         results_sim(r,:) = RMS_sim;
         
         % Increment repeat
         r = r + 1;
-%     end    
+     end    
 end
 close(dbox) 
 
